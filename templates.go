1d4fedd67ff051ba47de3e83cb0ed10b523ed9fc
Kevin Nowland <kevin.nowland@awarehq.com>
2024-08-09 10:04:07 -0400

    refactor: move template into separate file

---
+++ templates.go
@@ -0,0 +1,68 @@
+package main
+
+const pageTemplate = `
+{{define "PAGE"}}
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Markdown </title>
+  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+  <style>
+  div.nav {
+    border-style: hidden double hidden hidden;
+    width: 15%;
+    padding-left: 2.5%;
+    margin-top: 2.5%;
+    float: left;
+  }
+  div.data {
+    width: 65%;
+    padding-left: 2.5%;
+    padding-right: 10%;
+    margin-top: 2.5%;
+    float: right;
+  }
+  div.nav ul {
+    list-style-type: none;
+  }
+  div.nav li {
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  a:link {
+    color: black;
+  }
+  a:visited {
+    color: black;
+  }
+  a:hover {
+    color: black;
+    font-weight: bold;
+  }
+  a:active {
+    color: grey;
+    font-weight: bold;
+  }
+  </style>
+</head>
+<body>
+<div class="body">
+  <div class="nav">
+    <ul>
+      {{range $link := .Nav.Links}}
+      <li><a href="{{$link.Href}}">{{$link.Text}}</a></li>
+      {{end}}
+    </ul>
+  </div>
+  <div class="data">
+    {{.Data}}
+  </div>
+</div>
+</body>
+</html>
+{{end}}
+`
